3cdf74e6e7849b7fcd05be0c5242ff5221b16e4e
remittor <remittor@gmail.com>
2015-11-17 14:56:54 +0300

    Fix patches (new script)

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,3 +3,8 @@ add_lunch_combo cm_d9-userdebug
 add_lunch_combo cm_d9-user dist
 
 sh device/jsr/d9/patches/apply.sh
+croot
+
+rm -rf out/target/product/d9/obj/PACKAGING/target_files_intermediates
+rm -f out/target/product/d9/system/build.prop
+rm -f out/target/product/d9/root/default.prop 
