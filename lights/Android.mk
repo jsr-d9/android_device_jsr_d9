5a8debe67ffb32fb7419f59c4fde0e7dec8a1e66
remittor <remittor@gmail.com>
2015-11-17 14:53:36 +0300

    Fix lights library

--- lights/Android.mk
+++ lights/Android.mk
@@ -12,16 +12,15 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-ifneq ($(filter u8815,$(TARGET_DEVICE)),)
 LOCAL_PATH:= $(call my-dir)
-# HAL module implemenation, not prelinked and stored in
+# HAL module implemenation stored in
 # hw/<COPYPIX_HARDWARE_MODULE_ID>.<ro.board.platform>.so
 include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := lights.c
 
-LOCAL_PRELINK_MODULE := false
+#LOCAL_PRELINK_MODULE := false
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 
 LOCAL_SHARED_LIBRARIES := libhardware_legacy liblog
@@ -29,4 +28,4 @@ LOCAL_SHARED_LIBRARIES := libhardware_legacy liblog
 LOCAL_MODULE := lights.$(TARGET_BOARD_PLATFORM)
 
 include $(BUILD_SHARED_LIBRARY)
-endif
+
